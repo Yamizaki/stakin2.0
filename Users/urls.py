e30c84f11b79e5733a612188fb5cff1d3e716cbc
Yamizaki <Arakaki_18@outlook.com>
2025-03-03 14:47:57 -0500

    btn-form JavaScript added

--- Users/urls.py
+++ Users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include 
 from . import views
 urlpatterns = [
-    path('', views.profile, name='profile'),
+    path('', views.Profile.as_view(), name='profile'),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def profile(request):
-    return render(request, 'profile.html')
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+class Profile(LoginRequiredMixin, TemplateView):
+    template_name = "profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["current_user"] = self.request.user  # Pasamos el usuario autenticado al template
+        delta = self.request.user.last_login - self.request.user.date_joined
+        years = delta.days // 365
+        months = (delta.days % 365) // 30
+        days = (delta.days % 365) % 30
+        context["user_time"] = {
+            "years": years,
+            "months": months,
+            "days": days,
+        }
+        return context
+
